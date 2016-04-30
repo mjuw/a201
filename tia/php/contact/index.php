3ea1167183015541b33714592d0e0ddf1cb26ce4
_Tyler Waite_ <waite.tyler@gmail.com>
2016-04-30 14:42:53 -0700

    updated css link

--- tia/php/contact/index.php
+++ tia/php/contact/index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <title>Massage by Tyler - Tutorial - Agile Development</title>
   <meta name="viewport" content="width-device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/tia.css">
+  <link rel="stylesheet" type="text/css" href="../css/tia.css">
 
   
   </head>  
