c0d664382ccf50c0b060c6ebaf79ec351fb8674d
Michael Estes <michael.estes@gmail.com>
2016-04-30 13:39:04 -0700

    added Prices and external stylesheet

---
+++ tia/php/prices/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+  <meta charset="utf-8" />
+  <title>Massage by Tyler - Tutorial - Agile Development</title>
+  <meta name="viewport" content="width-device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../css/global.css" />
+  </head>  
+  <body>
+    <div id="wrapper">
+      <div id="callout">
+        <p>Call us at <b><a href="tel:2155555555">215-555-5555</a></b></p>
+      </div>
+      <header>
+        <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/massagelogo.png" alt="Massage by Tia" title="Massage by Tia"/></a>
+      </header>
+      <nav>
+        <ul>
+          <li class=""><a href="#">Home</a></li>
+          <li class=''><a href="#">Facility</a></li>
+          <li class=''><a href="#">Services</a></li>
+          <li class="active"><a href="#">Pricing</a></li>
+          <li class=''><a href="#">Hours</a></li>
+          <li class=''><a href="#">Contact</a></li>
+        </ul>
+        <div class="clearfix"></div>
+      </nav>
+      <div class="banner">
+        <img src="http://www.w3newbie.com/wp-content/uploads/massagebanner.png" alt="Massage by Tia, banner" title="Massage by Tia"/>
+      </div>
+      
+      <main>
+        <h3>Tia's Services and Prices</h3>
+        <ul>
+          <li>$35 for 20 minute basic massage or sauna session</li>
+          <li>Massage and Sauna prices for full hour (50 min) sessions: $75</li>
+          <li>Annual Membership $450</li>
+          <li>Members receive discounts and free sauna services </li>
+        </ul>
+      </main>
+      
+        <footer>
+          <div class="section">
+            <p>Massage by Tia</p>
+            <p><b>215-555-5555</b><br>
+            1700 Chestnut Street<br>
+            Philadelphia, PA 19100<br>
+            tia@massage.com
+            </p>
+          
+          </div>
+          
+          <div class="section">
+            <ul>
+              <li>
+                <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/facebook1.png" /></a>   </li>
+                <li> <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/googleplus.png" /></a>   </li>
+               <li>  <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/twitter1.png" /></a>   </li>
+               <li>  <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/youtube1.png" /></a>  
+              </li>
+            
+            </ul>
+          
+          </div>
+          
+          <div class="section">
+          <p>Connect With Us!</p>
+          <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/plant.png" /></a>  
+          </div>
+        </footer>
+    </div>
+    <p style="text-align: center; padding: 0px;">&#169; Copyright - Massage by Tia</p>
+    
+    
+    
+    
+    
+  </body>
+</html>
