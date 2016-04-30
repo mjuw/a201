8b1419d3361d5cbdaaaa9814221b9757ac3a0b1c
meenalsrivastva <srivam@uw.edu>
2016-04-30 13:43:08 -0700

    services page content was added

---
+++ tia/php/services/index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--Tyler Waite's HTML 5 assignment 4-18-2016-->  
+<head>
+  <meta charset="utf-8" />
+  <title>Massage by Tyler - Tutorial - Agile Development</title>
+  <meta name="viewport" content="width-device-width, initial-scale=1.0">
+  
+  <style>
+    body{
+      background: #F1F0D1;
+      font-family: Verdana, Tahoma, Arial, sans-serif;
+      font-size: 18px;
+      overflow: auto;
+    }
+    h1, h2, h3 {
+      text-align: center;
+      padding-left: 5%;
+      color: #878e63;
+    }
+    
+    p {
+      padding: 2%;
+      color: #878e63;
+    }
+  
+    img {
+      text-align: center;
+      max-width: 100%;
+      height: auto;
+      width: auto;
+    }
+    
+    #wrapper {
+      margin: 0 auto;
+      max-width: 1020px;
+      width: 98%;
+      background: #FEFBE8;
+      border: 1px solid #878e63;
+      border-radius: 2px;
+      box-shadow: 0 0 10px 0px rgba(12, 3, 25, 0.8);
+    }
+    
+    #callout {
+      width: 100%;
+      height: auto;
+      background: #878e63;
+      overflow: hidden;
+      overflow: hidden;
+    }
+    
+    #callout p {
+      text-align: right;
+      font-size: 13px;
+      padding: 0.1% 5% 0 0;
+      color: #F1F0D1;
+      font-family: Verdana, Tahoma, Arial, sans-serif;
+    }
+    
+    #callout p a {
+      color: #f1f0d1;
+      text-decoration: none;
+    }
+    
+    header {
+      width: 96%;
+      min-height: 125px;
+      padding: 5px;
+      text-align: center;
+    }
+    
+    nav ul {
+      list-style: none;
+      margin: 0;
+      padding-left: 50px;
+    }
+    
+    nav ul li {
+      float: left;
+      border: 1px solid #878e63;
+      width: 15%;
+    }
+    
+    nav ul li a {
+      background: #f1f0d1;
+      display: block;
+      padding: 5% 12%;
+      font-weight: bold;
+      font-size: 18px;
+      color:#878e63;
+      text-decoration: none;
+      text-align: center;
+    }
+    
+    nav ul li a:hover, nav ul li.active a {
+      background-color: #878e63;
+      color: #F1F0D1;
+    }
+    
+    .banner img {
+      width: 100%;
+      border-top: 1px solid #878e63;
+      border-bottom: 1px solid #878e63;
+    }
+    
+    .clearfix {
+      clear: both;
+    }
+    
+    .left-col {
+      width: 55%;
+      float: left;
+      margin: -2% 1% 1% 1%;
+    }
+    
+    .sidebar {
+      width: 40%;
+      float: right;
+      margin: 1%;
+      text-align: center;
+    
+    }
+    
+    .therapy {
+      float: left;
+      margin: 0 auto;
+      width: 100%;
+      height: auto;
+      padding: 1%;
+    
+    }
+    
+    .section {
+      width: 29%;
+      float: left;
+      margin: 2% 2%;
+      text-align: center;
+    }
+    
+    footer {
+      background: #878e63;
+      width: 100%;
+      overflow: hidden;
+    
+    } 
+    
+    footer p, footer h3 {
+      color: #f1f0d1;
+    
+    }
+    
+    footer p a {
+      color: #f1f0d1;
+      text-decoration: none;
+    }
+    
+    ul {
+      list-style:none;
+      margin: 0;
+      padding: 0;
+    }
+    
+    li {
+      display: inline;
+      
+    }
+    
+    ul li img {
+      height: 50px;
+    }
+    
+    /*-----------Media!!---------------*/
+    @media screen and (max-width: 478px) {
+      body {
+        font-size: 13px;
+      }
+    }
+    @media screen and (max-width: 740px) {
+      nav {
+        width: 100%;
+        margin-bottom: 10px;
+      }
+      
+      nav ul{
+        list-style: none;
+        margin: 0;
+        padding-left: 0;
+        
+      }
+      
+      nav ul li {
+        text-align: center;
+        margin-left: 0 auto;
+        width: 100%;
+        border-top: 1px solid #878e63;
+        border-right: 0px solid #878e63;
+        border-bottom: 1px solid #878e63;
+        border-left: 0px solid #878e63;
+      }
+      nav ul li a {
+        padding: 8px 0;
+        font-size: 16px;
+      }
+      .left-col {
+        width: 100%
+      }
+      .sidebar {
+        width: 100%
+      }
+      .section{
+        float: left;
+        width: 100%;
+        margin: 0;
+        
+      }
+    }
+  </style>
+  
+  </head>  
+  <body>
+    <div id="wrapper">
+      <div id="callout">
+        <p>Call us at <b>215-555-5555</b></p>
+      </div>
+      <header>
+        <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/massagelogo.png" alt="Massage by Tia" title="Massage by Tia"/></a>
+      </header>
+      <nav>
+        <ul>
+          <li class=''><a href="#">Home</a></li>
+          <li class=''><a href="#">Facility</a></li>
+          <li class='active'><a href="#">Services</a></li>
+          <li class=''><a href="#">Pricing</a></li>
+          <li class=''><a href="#">Hours</a></li>
+          <li class=''><a href="#">Contact</a></li>
+        </ul>
+        <div class="clearfix"></div>
+      </nav>
+      <div class="banner">
+        <img src="http://www.w3newbie.com/wp-content/uploads/massagebanner.png" alt="Massage by Tia, banner" title="Massage by Tia"/>
+      </div>
+      
+      <center><img src="http://www.w3newbie.com/wp-content/uploads/three_sayings.png" alt="Massage by Tia, banner" title="Massage by Tia"/></center>
+      
+      <section class="left-col">
+        <p style="text-indent: 50px;"><h3>Swedish Massage Therapy</h3>
+			<ul>
+				<li>This is the most common type of massage therapy in the United States. It is also known as Swedish massage or simply massage therapy.</li>
+				<li>Massage therapists use long smooth strokes, kneading, and circular movements on superficial layers of muscle using massage lotion or oil.</li>
+				<li>Swedish massage therapy can be very gentle and relaxing.</li>
+				<li>If you've never had massage before, this is a good one to try first.</li>
+		</p>
+
+		<p style="text-indent: 50px;"><h3>Aromatherapy Massage</h3>
+			<br/>Aromatherapy massage is massage therapy with the addition of one or more scented plant oils called essential oils to address specific needs.
+			<br/>The massage therapist can select oils that are relaxing, energizing, stress-reducing, balancing, etc. One of the most common essential oils used in aromatherapy massage is lavender.
+			<br/>Aromatherapy massage is particularly suited to stress-related conditions or conditions with an emotional component.
+			<br/>Find out more about hot stone massage.
+		</p>
+		
+		<p style="text-indent: 50px;"><h3>Hot Stone Massage</h3>
+			<br/>Heated, smooth stones are placed on certain points on the body to warm and loosen tight muscles and balance energy centers in the body.
+			<br/>The massage therapist may also hold stones and apply gentle pressure with them. The warmth is comforting. Hot stone massage is good for people who have muscle tension but prefer lighter massage.
+		</p>
+		
+		<p style="text-indent: 50px;"><h3>Deep Tissue Massage</h3>
+			<br/>Deep tissue massage targets the deeper layers of muscle and connective tissue. The massage therapist uses slower strokes or friction techniques across the grain of the muscle.
+			<br/>Deep tissue massage is used for chronically tight or painful muscles, repetitive strain, postural problems, or recovery from injury.
+			<br/>People often feel sore for one to two days after deep tissue massage. Learn more about this style of massage: Deep Tissue Massage.
+		</p>
+		
+		
+		<p style="text-indent: 50px;"><h3>Shiatsu</h3>
+			<br/>Shiatsu is a form of Japanese bodywork that uses localized finger pressure in a rhythmic sequence on acupuncture meridians.
+			<br/>Each point is held for for two to eight seconds to improve the flow of energy and help the body regain balance.
+			<br/>People are normally pleasantly surprised when they try shiatsu for the first time. It is relaxing yet the pressure is firm, and there is usually no soreness afterwards. Find out more about shiatsu.
+		</p>
+
+
+		<p style="text-indent: 50px;"><h3>Thai Massage</h3>
+			<br/>Like shiatsu, Thai massage aligns the energies of the body using gentle pressure on specific points. Thai massage also includes compressions and stretches.
+			<br/>You don't just lie there--the therapist moves and stretches you into a sequence of postures. It's like yoga without doing any work. Thai massage is more energizing than other forms of massage. It is also reduces stress and improves flexibility and range of motion.
+			<br/>Find out more about thai massage. Also, see photos of a Thai massage session.
+		</p>
+	
+		<p style="text-indent: 50px;"><h3>Pregnancy Massage</h3>
+			<ul>
+				<li>Also called prenatal massage, pregnancy massage is becoming increasingly popular with expectant mothers. Massage therapists who are certified in pregnancy massage know the proper way to position and support the woman's body during the massage, and how to modify techniques.</li>
+				<li>Pregnancy massage is used to reduce stress, decrease swelling, relieve aches and pains, and reduce anxiety and depression. The massage is customized to a woman's individual needs. Learn more about pregnancy massage.</li>
+		</p>
+	
+	
+		<p style="text-indent: 50px;"><h3>Reflexology</h3>
+			<ul>
+				<li>Although reflexology is sometimes called foot massage, it is more than simple foot massage. Reflexology involves applying pressure to certain points on the foot that correspond to organs and systems in the body. Reflexology is very relaxing, especially for people who stand on their feet all day or just have tired, achy feet. Learn more about reflexology.</li>
+		</p>
+	
+
+		<p style="text-indent: 50px;"><h3>Sports Massage</h3>
+			<ul>
+				<li>Sports massage is specifically designed for people who are involved in physical activity. But you don't have to be a professional athlete to have one-it's also used by people who are active and work out often. The focus isn't on relaxation but on preventing and treating injury and enhancing athletic performance.</li>
+				<li>A combination of techniques are used. The strokes are generally faster than Swedish massage. Facilitated stretching is a common technique. It helps to loosen muscles and increase flexibility.</li>
+		</p>
+	
+		<p style="text-indent: 50px;"><h3>Back Massage</h3>
+			<ul>
+				<li>Some massage clinics and spas offer 30-minute back massages. If a back massage is not expressly advertised, you can also book a 30- or 40-minute massage and ask that the massage therapist to focus on your back.</li>
+		</p>
+
+
+		<p style="text-indent: 50px;"><h3>Bowen Massage</h3>
+			<ul>
+				<li>The Bowen Technique uses a series of simple, gentle moves across muscle and connective tissue.</li>
+				<li>It is similar to tuning a stringed instrument that sends harmonic vibrations that balance the body.</li>
+				<li>Because it works in harmony with the body, the Bowen Technique is incredibly effective for any muscular, skeletal, or nerve imbalance.</li>
+				<li>It has also been successful in treating chronic pain due to injury.</li>
+				<li>The technique is so gentle it can be used on anyone from infants to the elderly.</li>
+				<li>It produces a deep relaxation and can release blocked emotional energy, which can accelerate the healing process.</li>
+		</p>
+	
+      </section>
+      <aside class="sidebar">
+        <div class="therapy">
+          <img src="http://www.w3newbie.com/wp-content/uploads/therapist.jpg" alt="therapist graphic" title="therapy"/>
+        </div>
+      </aside>
+     <div class="clearfix"></div> 
+        <div class="section">
+          <h3>Private Massage Area</h3>
+          <img src="http://www.w3newbie.com/wp-content/uploads/private.png" alt="private massage graphic" title="therapy"/>
+        <p>Morbi ullamcorper purus arcu, et hendrerit leo condimentum vel. Ut id ultrices nisl, quis bibendum purus. Maecenas vel leo nec metus consequat vehicula non nec nibh. Ut dignissim nibh eu tellus tristique pulvinar vel at nisl. Duis id rutrum est. Suspendisse risus neque, euismod maximus tempus et, viverra ultricies justo. Sed in mollis ligula. Phasellus aliquet magna sed mollis molestie. Etiam eget porta velit. Nulla eros nulla, convallis sed maximus in, sagittis non arcu. Nam eu condimentum massa, sed cursus felis. Morbi interdum porta odio eu accumsan. Mauris lobortis risus ac ante luctus, sed rutrum velit ultrices. Sed imperdiet tortor ut magna interdum mattis.</p>
+        </div>
+        
+        <div class="section">
+          <h3>Sauna and Steam Room</h3>
+          <img src="http://www.w3newbie.com/wp-content/uploads/sauna-steam.png" alt="sauna graphic" title="therapy"/>
+          <p>Maecenas pretium malesuada nisi eget porttitor. Pellentesque urna ipsum, commodo vel felis at, hendrerit mollis erat. Nunc consequat lobortis risus id dapibus. Curabitur ullamcorper a orci eu ultrices. Vestibulum faucibus venenatis semper. Nulla facilisi. Aenean dignissim magna nec felis sodales ullamcorper. Sed eget nibh condimentum felis placerat blandit vel in nulla. Suspendisse potenti.</p>
+
+<p>Curabitur blandit ante felis, eu placerat augue malesuada ut. Donec tincidunt vel sem non venenatis. Aliquam leo lacus, scelerisque vel tortor at, accumsan sagittis augue. Sed gravida ante vitae sem ornare ultrices. In fermentum nulla eu quam pulvinar feugiat sodales sed risus. Nunc et scelerisque lorem, vel eleifend dui. Sed vitae condimentum est, ac auctor velit</p>
+        </div>
+        
+        <div class="section">
+          <h3>Our Resort Retreat</h3>
+          <img src="http://www.w3newbie.com/wp-content/uploads/retreat.png" alt="retreat graphic" title="therapy"/>
+                    <p>Aliquam bibendum arcu vel purus lobortis, a suscipit ipsum rhoncus. Nullam eget risus lobortis, egestas nunc facilisis, convallis felis. Mauris nisi augue, elementum vel pretium et, eleifend sit amet eros. Cras tortor sapien, fringilla sit amet risus sed, imperdiet porta velit. Suspendisse tellus nulla, blandit in velit sed, eleifend ullamcorper nibh. Quisque gravida sapien in ornare ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer aliquet gravida nunc, in facilisis leo faucibus at. Vivamus massa erat, viverra id augue nec, vestibulum varius dolor. Sed ac enim vitae dui accumsan suscipit non eu leo. In sollicitudin, risus eget auctor varius, est libero scelerisque magna, quis rutrum dolor massa eget neque. Vivamus blandit, magna vitae vehicula ornare, augue quam varius ligula, et porta risus augue nec arcu. Suspendisse augue risus, laoreet id ultrices non, dignissim non nibh. Morbi suscipit faucibus lacus ut tempor. Donec at ex eu justo semper semper vitae eu eros. Sed at neque in quam cursus sollicitudin eget a dui.</p>
+
+        </div>
+        
+        <center><img src="http://www.w3newbie.com/wp-content/uploads/ease.png" /></center>
+      
+        <footer>
+          <div class="section">
+            <p>Massage by Tia</p>
+            <p><b>215-555-5555</b><br>
+            1700 Chestnut Street<br>
+            Philadelphia, PA 19100<br>
+            tia@massage.com
+            </p>
+          
+          </div>
+          
+          <div class="section">
+            <ul>
+              <li>
+                <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/facebook1.png" /></a>   </li>
+                <li> <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/googleplus.png" /></a>   </li>
+               <li>  <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/twitter1.png" /></a>   </li>
+               <li>  <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/youtube1.png" /></a>  
+              </li>
+            
+            </ul>
+          
+          </div>
+          
+          <div class="section">
+          <p>Connect With Us!</p>
+          <a href="#"><img src="http://www.w3newbie.com/wp-content/uploads/plant.png" /></a>  
+          </div>
+        </footer>
+    </div>
+    <p style="text-align: center; padding: 0px;">&#169; Copyright - Massage by Tia</p>
+    
+    
+    
+    
+    
+  </body>
+</html>
