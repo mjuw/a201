0c1a6e3de06adc70c7ab1972f3faab021d5a1ac1
Todd Rowan <todd.rowan@gmail.com>
2016-04-30 14:54:48 -0700

    More hours updates.

--- tia/php/hours/index.php
+++ tia/php/hours/index.php
@@ -3,11 +3,10 @@
 <!--Tyler Waite's HTML 5 assignment 4-18-2016-->  
 <head>
   <meta charset="utf-8" />
-  <title>Massage by Tyler - Tutorial - Agile Development</title>
+  <title>Massage by Tia - Tutorial - Agile Development</title>
   <meta name="viewport" content="width-device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/tia.css">
-
   
+  <link rel="stylesheet" type="text/css" href="../css/tia.css">
   </head>  
   <body>
     <div id="wrapper">
@@ -19,9 +18,9 @@
       </header>
       <nav>
         <ul>
-          <li class='active'><a href="/">Home</a></li>
+          <li class=''><a href="/">Home</a></li>
           <li class=''><a href="/facility">Facility</a></li>
-          <li class=''><a href="/services">Services</a></li>
+          <li class='active'><a href="/services">Services</a></li>
           <li class=''><a href="/pricing">Pricing</a></li>
           <li class=''><a href="/hours">Hours</a></li>
           <li class=''><a href="/contact">Contact</a></li>
@@ -35,21 +34,15 @@
       <center><img src="http://www.w3newbie.com/wp-content/uploads/three_sayings.png" alt="Massage by Tia, banner" title="Massage by Tia"/></center>
       
       <section class="left-col">
-        <p style="text-indent: 50px;"></p>
-        
-        <p style="text-indent: 50px;"></p>
-        
-      </section>
-      <aside class="sidebar">
-        <h1>Hours page</h1>
+        <div style="text-indent: 50px;"><h1>Hours page</h1>
         <p>Normal Business Hours</p>
 		<ul>
 			<li>Mon - Sat: 9am - 7pm</li>
 			<li>Closed Sundays</li>
 		</ul>
-	<p>Hours are reduced during "Massage By Tia Resort Retreats" due to limited staff availability.</p>
+	<p>Hours are reduced during "Massage By Tia Resort Retreats" due to limited staff availability.</p></div>
 
-<p>Facility hours during retreats:</p>
+<div style="text-indent: 50px;"><p>Facility hours during retreats:</p>
 	<ul>
 	<li>Tuesday - Sat: 9am - 1pm, 3pm - 7pm</li>
 	<li>Closed Sundays</li>
@@ -68,7 +61,38 @@
 	<li>Independence Day</li>
 	<li>Memorial Day</li>
 	<li>Labor Day</li>
-   </ul>     
+   </ul>
+   </div>
+
+
+      </section>
+      <aside class="sidebar">
+        <div class="therapy">
+          <img src="http://www.w3newbie.com/wp-content/uploads/therapist.jpg" alt="therapist graphic" title="therapy"/>
+        </div>
+      </aside>
+     <div class="clearfix"></div> 
+        <div class="section">
+          <h3>Private Massage Area</h3>
+          <img src="http://www.w3newbie.com/wp-content/uploads/private.png" alt="private massage graphic" title="therapy"/>
+        <p>Morbi ullamcorper purus arcu, et hendrerit leo condimentum vel. Ut id ultrices nisl, quis bibendum purus. Maecenas vel leo nec metus consequat vehicula non nec nibh. Ut dignissim nibh eu tellus tristique pulvinar vel at nisl. Duis id rutrum est. Suspendisse risus neque, euismod maximus tempus et, viverra ultricies justo. Sed in mollis ligula. Phasellus aliquet magna sed mollis molestie. Etiam eget porta velit. Nulla eros nulla, convallis sed maximus in, sagittis non arcu. Nam eu condimentum massa, sed cursus felis. Morbi interdum porta odio eu accumsan. Mauris lobortis risus ac ante luctus, sed rutrum velit ultrices. Sed imperdiet tortor ut magna interdum mattis.</p>
+        </div>
+        
+        <div class="section">
+          <h3>Sauna and Steam Room</h3>
+          <img src="http://www.w3newbie.com/wp-content/uploads/sauna-steam.png" alt="sauna graphic" title="therapy"/>
+          <p>Maecenas pretium malesuada nisi eget porttitor. Pellentesque urna ipsum, commodo vel felis at, hendrerit mollis erat. Nunc consequat lobortis risus id dapibus. Curabitur ullamcorper a orci eu ultrices. Vestibulum faucibus venenatis semper. Nulla facilisi. Aenean dignissim magna nec felis sodales ullamcorper. Sed eget nibh condimentum felis placerat blandit vel in nulla. Suspendisse potenti.</p>
+
+<p>Curabitur blandit ante felis, eu placerat augue malesuada ut. Donec tincidunt vel sem non venenatis. Aliquam leo lacus, scelerisque vel tortor at, accumsan sagittis augue. Sed gravida ante vitae sem ornare ultrices. In fermentum nulla eu quam pulvinar feugiat sodales sed risus. Nunc et scelerisque lorem, vel eleifend dui. Sed vitae condimentum est, ac auctor velit</p>
+        </div>
+        
+        <div class="section">
+          <h3>Our Resort Retreat</h3>
+          <img src="http://www.w3newbie.com/wp-content/uploads/retreat.png" alt="retreat graphic" title="therapy"/>
+                    <p>Aliquam bibendum arcu vel purus lobortis, a suscipit ipsum rhoncus. Nullam eget risus lobortis, egestas nunc facilisis, convallis felis. Mauris nisi augue, elementum vel pretium et, eleifend sit amet eros. Cras tortor sapien, fringilla sit amet risus sed, imperdiet porta velit. Suspendisse tellus nulla, blandit in velit sed, eleifend ullamcorper nibh. Quisque gravida sapien in ornare ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer aliquet gravida nunc, in facilisis leo faucibus at. Vivamus massa erat, viverra id augue nec, vestibulum varius dolor. Sed ac enim vitae dui accumsan suscipit non eu leo. In sollicitudin, risus eget auctor varius, est libero scelerisque magna, quis rutrum dolor massa eget neque. Vivamus blandit, magna vitae vehicula ornare, augue quam varius ligula, et porta risus augue nec arcu. Suspendisse augue risus, laoreet id ultrices non, dignissim non nibh. Morbi suscipit faucibus lacus ut tempor. Donec at ex eu justo semper semper vitae eu eros. Sed at neque in quam cursus sollicitudin eget a dui.</p>
+
+        </div>
+        
         <center><img src="http://www.w3newbie.com/wp-content/uploads/ease.png" /></center>
       
         <footer>
