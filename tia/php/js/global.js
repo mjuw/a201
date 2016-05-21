ed3ebff5be3c99a6d407c5fab481a8b64b111175
Michael Estes <michael.estes@gmail.com>
2016-05-21 13:13:11 -0700

    adding contact page and maps integration

--- tia/php/js/global.js
+++ tia/php/js/global.js
@@ -2,11 +2,26 @@ $(document).ready(function(){
 	global.storesBanner('div#store-hours-banner');
 });
 $(window).load(function(){
-
+	if( $('#google-map') ){ global.googleMap('google-map'); }
 });
 
 
 let global = {
+	googleMap : function(locId){
+		let latLng = {lat: 47.6678999, lng: -122.0964889};
+		let map = new google.maps.Map(document.getElementById(locId), {
+		    center: latLng,
+		    zoom: 13,
+		    zoomControl: false,
+	    	scaleControl: true,
+	    	streetViewControl: true
+		  });
+		let marker = new google.maps.Marker({
+		    position: latLng,
+		    map: map,
+		    title: 'Massage By Tia'
+		  });
+	},
 	storesBanner : function(locId){
 		let html = Mustache.render(mst.stores,{"status" : "Open Now","hours" : "8am - 7pm"});
 		$(locId).html(html);
